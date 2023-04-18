162a72b5b1de39ecad6cf740d41ec6ea37525f4f
Felix Schmitz <felix.schmitz@igs-beratung.de>
2023-04-18 20:04:31 +0200

    update

---
+++ Vorlesung_4/Übungsaufgaben.py
@@ -0,0 +1,27 @@
+import random
+
+def aufgabe_15():
+    range = int(input("Bitte gib eine Seitenzahl des Würfels ein: "))
+    match range:
+        case 4:
+            option = random.randrange(1,4)
+            print(f"Deine Würfelwurf: {option}")
+        case 6:
+            option = random.randrange(1,6)
+            print(f"Deine Würfelwurf: {option}")
+        case 8:
+            option = random.randrange(1,8)
+            print(f"Deine Würfelwurf: {option}")
+        case 10:
+            option = random.randrange(1,10)
+            print(f"Deine Würfelwurf: {option}")
+        case 12:
+            option = random.randrange(1,12)
+            print(f"Deine Würfelwurf: {option}")
+        case 20:
+            option = random.randrange(1,20)
+            print(f"Deine Würfelwurf: {option}")
+        case default:
+            print("Bitte gib eien Seitenzahl von den genannten an: 4, 6, 8, 10, 12, 20")
+
+aufgabe_15()
