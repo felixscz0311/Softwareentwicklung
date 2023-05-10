19362ef228f680a46e9899698521c2a61db8a8f1
Felix Schmitz <felix.schmitz@igs-beratung.de>
2023-05-10 21:06:45 +0200

    update

---
+++ Nacharbeit/Datentypen.py
@@ -0,0 +1,11 @@
+# Alle Datentypen
+x = "Hello, world!" #string
+x = 20 #integer
+x = 20.5 #float
+x = 1j #complex
+x = ["Apfel", "Bananen", "Kirschen"] #liste
+x = ("Apfel", "Bananen", "Kirschen") #tuple
+x = range(6) #range
+x = {"name" : "John", "age" : 36} #dictornary
+x = True #boolean
+x = b"Hello" #bites
